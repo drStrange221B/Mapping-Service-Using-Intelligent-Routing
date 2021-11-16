7b9d39d34a05fefb916af4e89baf7c30d57024ff
drStrange221B <50057970+drStrange221B@users.noreply.github.com>
2021-11-16 15:57:56 -0500

    service discovery init

--- Spring-cloud-service-discovery/src/test/java/com/example/demo/SpringCloudServiceDiscoveryApplicationTests.java
+++ Spring-cloud-service-discovery/src/test/java/com/example/demo/SpringCloudServiceDiscoveryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.emrys;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceZuulApplicationTests {
+class SpringCloudServiceDiscoveryApplicationTests {
 
 	@Test
 	void contextLoads() {
