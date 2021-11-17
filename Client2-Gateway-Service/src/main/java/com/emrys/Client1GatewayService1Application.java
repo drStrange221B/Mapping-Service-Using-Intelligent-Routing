a4d8b471292f9c7b306edc2df4a070468e26cd6c
drStrange221B <50057970+drStrange221B@users.noreply.github.com>
2021-11-17 17:03:30 -0500

    Client-B init

--- Client2-Gateway-Service/src/main/java/com/emrys/Client1GatewayService1Application.java
+++ Client2-Gateway-Service/src/main/java/com/emrys/Client1GatewayService1Application.java
@@ -3,26 +3,26 @@ package com.emrys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
-public class Client1GatewayServiceApplication {
+public class Client1GatewayService1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Client1GatewayServiceApplication.class, args);
+		SpringApplication.run(Client1GatewayService1Application.class, args);
 	}
 
 	@RequestMapping(value = "/")
-	public String hello(@RequestHeader("x-location") String location) {
-		return "Hello " + location + " !";
+	public String goodbye() {
+		return "Good Bye";
 	}
 
 	@RequestMapping(value = "/hey")
 	public String heyHello() {
-		return "Hey Hello";
+		return "Hey Good Bye";
 	}
+
 }
