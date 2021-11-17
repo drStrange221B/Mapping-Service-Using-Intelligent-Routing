4550aeaa4b5fc10946ed7d8dc7ea24dcbc42bf98
drStrange221B <50057970+drStrange221B@users.noreply.github.com>
2021-11-17 17:02:32 -0500

    client-A init

--- Client1-Gateway-Service/src/test/java/com/emrys/Client1GatewayServiceApplicationTests.java
+++ Client1-Gateway-Service/src/test/java/com/emrys/Client1GatewayServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceZuulApplicationTests {
+class Client1GatewayServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
